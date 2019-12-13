46a2bad7d4ea708a28f5143b52c2c98f8fcbc0f5
panliangxiao <panliangxiao@58.com>
2019-12-13 18:59:37 +0800

    视屏转码压缩

--- app/src/main/java/com/android/app/luping/MainActivity.java
+++ app/src/main/java/com/android/app/luping/MainActivity.java
@@ -1,10 +1,17 @@
 package com.android.app.luping;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.plx.android.app.capturescreen.ScreenRecorderActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -15,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.screen_recorder);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenRecorderActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
