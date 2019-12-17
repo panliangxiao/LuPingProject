764e4a721fabef9601c17e0f201f30e782a56d76
panliangxiao <panliangxiao@58.com>
2019-12-17 19:33:49 +0800

    视屏转码压缩

--- capturescreen/src/main/java/com/plx/android/app/capturescreen/service/ScreenRecorderService.java
+++ capturescreen/src/main/java/com/plx/android/app/capturescreen/service/ScreenRecorderService.java
@@ -31,8 +31,6 @@ public class ScreenRecorderService extends Service {
     private int screenHeight;
     private int screenDensity;
 
-    private Context context = null;
-
 
     @Override
     public IBinder onBind(Intent intent) {
