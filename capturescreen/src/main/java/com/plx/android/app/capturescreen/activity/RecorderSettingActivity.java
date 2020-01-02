6bd0f98c7be3fd3afcdd69d81a6cf485005c6298
panliangxiao <panliangxiao@58.com>
2020-01-02 17:23:51 +0800

    屏幕录制

--- capturescreen/src/main/java/com/plx/android/app/capturescreen/activity/RecorderSettingActivity.java
+++ capturescreen/src/main/java/com/plx/android/app/capturescreen/activity/RecorderSettingActivity.java
@@ -1,6 +1,16 @@
 package com.plx.android.app.capturescreen.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.plx.android.app.capturescreen.R;
+
 public class RecorderSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sr_recorder_setting_act);
+    }
 }
